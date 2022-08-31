c0295f5b8dee488516019d71ec85ed95ed8e3d24
Gwhyyy <ffikhi.aanas@gmail.com>
2022-08-31 04:27:03 +0100

    [Add, Edit], semantics labels, fix the timeOfPublish show

--- lib/screens/home_screen/widgets/IURPostCard.dart
+++ lib/screens/home_screen/widgets/IURPostCard.dart
@@ -15,51 +15,63 @@ class IURPostCard extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 15),
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundImage: AssetImage(post.authorImgPath),
-                  ),
-                  const SizedBox(width: 10),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AutoSizeText(
-                        post.authorName,
-                        style: Theme.of(context).textTheme.headline3,
+          Semantics(
+            label: "general information about post, authorName, authorImg",
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Semantics(
+                      label: "author image",
+                      child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: AssetImage(post.authorImgPath),
                       ),
-                      AutoSizeText(
-                        post.authorName,
-                        style: Theme.of(context).textTheme.subtitle1,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  SvgIconsHelper.fromSvg(
-                    svgPath: "assets/icons/send.svg",
-                    semanticLabel: "send icon",
-                    size: 25,
-                    color: Theme.of(context).primaryColor.withOpacity(.6),
-                  ),
-                  const SizedBox(width: 10),
-                  SvgIconsHelper.fromSvg(
-                    svgPath: "assets/icons/items.svg",
-                    semanticLabel: "items icon",
-                    size: 20,
-                    color: Theme.of(context).primaryColor.withOpacity(.6),
-                  ),
-                ],
-              ),
-            ],
+                    ),
+                    const SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Semantics(
+                          label: "author name",
+                          child: AutoSizeText(
+                            post.authorName,
+                            style: Theme.of(context).textTheme.headline3,
+                          ),
+                        ),
+                        Semantics(
+                          label: "time of publish differance from now",
+                          child: AutoSizeText(
+                            "${DateTime.now().difference(post.timeOfPublish).inMinutes} min ago",
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SvgIconsHelper.fromSvg(
+                      svgPath: "assets/icons/send.svg",
+                      semanticLabel: "send icon",
+                      size: 25,
+                      color: Theme.of(context).primaryColor.withOpacity(.6),
+                    ),
+                    const SizedBox(width: 10),
+                    SvgIconsHelper.fromSvg(
+                      svgPath: "assets/icons/items.svg",
+                      semanticLabel: "items icon",
+                      size: 20,
+                      color: Theme.of(context).primaryColor.withOpacity(.6),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
